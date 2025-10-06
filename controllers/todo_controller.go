1609edd8e1a2994eabbe811227adb2536fc9daa4
visi8-dudi <dudi.ali@visi8.com>
2025-10-06 17:09:58 +0700

    integrate with sqlite

--- controllers/todo_controller.go
+++ controllers/todo_controller.go
@@ -1,74 +1,56 @@
 package controllers
 
 import (
+	"mini_backend/config"
 	"mini_backend/models"
 	"net/http"
 	"strconv"
-	"sync"
 
 	"github.com/gin-gonic/gin"
 )
 
-var (
-	Todos  []models.Todo
-	NextID = 1
-	mu     sync.Mutex
-)
-
 func GetTodos(c *gin.Context) {
-	mu.Lock()
-	defer mu.Unlock()
-
-	c.JSON(http.StatusOK, Todos)
+	var todos []models.Todo
+	config.DB.Find(&todos)
+	c.JSON(http.StatusOK, todos)
 }
 
 func CreateTodos(c *gin.Context) {
-	mu.Lock()
-	defer mu.Unlock()
-
 	var todo models.Todo
+
 	if err := c.ShouldBindJSON(&todo); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error ": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	todo.ID = NextID
-	NextID++
-	Todos = append(Todos, todo)
+	config.DB.Create(&todo)
 	c.JSON(http.StatusOK, todo)
 }
 
 func UpdateTodos(c *gin.Context) {
-	mu.Lock()
-	defer mu.Unlock()
-
 	id, _ := strconv.Atoi(c.Param("id"))
+	var todo models.Todo
 
-	for i, t := range Todos {
-		if t.ID == id {
-			Todos[i].Completed = !t.Completed
-			c.JSON(http.StatusOK, Todos[i])
-
-			return
-		}
+	if err := config.DB.First(&todo, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Todo not found"})
+		return
 	}
 
-	c.JSON(http.StatusNotFound, gin.H{"error": "Todo not found"})
+	todo.Completed = !todo.Completed
+	config.DB.Save(&todo)
+
+	c.JSON(http.StatusOK, todo)
 }
 
 func DeleteTodos(c *gin.Context) {
-	mu.Lock()
-	defer mu.Unlock()
-
 	id, _ := strconv.Atoi(c.Param("id"))
+	var todo models.Todo
 
-	for i, t := range Todos {
-		if t.ID == id {
-			Todos = append(Todos[:i], Todos[i+1:]...)
-			c.JSON(http.StatusOK, gin.H{"message": "Todo deleted"})
-			return
-		}
+	if err := config.DB.First(&todo, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Todo not found"})
+		return
 	}
 
-	c.JSON(http.StatusNotFound, gin.H{"error": "Todo not Found"})
+	config.DB.Delete(&todo)
+	c.JSON(http.StatusOK, gin.H{"message": "Todo deleted"})
 }
